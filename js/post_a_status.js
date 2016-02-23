1d71495560b2fddfb0f6427fd4474a31a68da734
Mason <mason.e.fish@gmail.com>
2016-02-22 20:01:19 -0800

    upload post button

--- js/post_a_status.js
+++ js/post_a_status.js
@@ -1,1 +1,12 @@
-placeholder
+document.addEventListener("DOMContentLoaded", function () {
+    var veil = document.getElementById('screenContent');
+    document.getElementById('tweetButton').addEventListener('click', function () {
+        if (veil.className == 'hideTweet') {
+            veil.className = '';
+        }
+    })
+
+    document.getElementById('closeButton').addEventListener('click', function (event) {
+        veil.className = 'hideTweet';
+    })
+});
