a50d561f64a413a86776db1cfd9d2014bf3f084e
Mason <mason.e.fish@gmail.com>
2016-03-21 14:15:17 -0700

    add status button, homogenize post button

--- js/reply.js
+++ js/reply.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var icons = document.querySelectorAll(".replyIcon");
     for (x = 0; x < icons.length; x++) {
         icons[x].addEventListener("click", function (e) {
-            var box = document.querySelector('#' + this.dataset.id);
+            var box = this.parentElement.parentElement.parentElement.nextElementSibling;
             box.toggle('showBox');
         });
     }
