f8175497017c235d66e12796db8c75fbd6fbeb1a
Mason <mason.e.fish@gmail.com>
2016-03-21 17:01:54 -0700

    final mandatory task with custom ajaxGet() function

--- js/load_more.js
+++ js/load_more.js
@@ -2,13 +2,21 @@ window.onload = function () {
     event_setup();
 }
 
+
+
 function load_more(res) {
   var x = document.getElementById("loadMore");
   x.onclick = function() {
-    ajaxGet("/statuses-1.html", function(res) {
-      document.getElementById("moreHere").innerHTML = res;
-      event_setup();
-    });
+    x.disabled = true;
+    x.toggle('offButton');
+    window.setTimeout(function() {
+      ajaxGet("/statuses-1.html", function(res) {
+        document.getElementById("moreHere").innerHTML = res;
+        event_setup();
+      });
+      x.disabled = false;
+      x.toggle('offButton');
+    }, 2000);
   }
 }
 
